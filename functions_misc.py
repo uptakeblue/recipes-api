70e480f9e44998b76d713be6319d2a85c70ddb22
Michael Rubin <github@uptakeblue.com>
2024-01-30 09:41:53 -0800

    added secretkey requirement to authorization endpoint

--- functions_misc.py
+++ functions_misc.py
@@ -13,6 +13,17 @@ MODULE = "functions_misc"
 def authenticate_POST(util: gu.Global_Utility, requestBody: dict):
     response = None
     try:
+        if (
+            "secretkey" not in requestBody
+            or requestBody["secretkey"] != util.settings["secretkey"]
+        ):
+            raise gu.UptakeblueException(
+                Exception("Unauthorized"),
+                source=f"{MODULE}.authenticate_POST()",
+                paramargs=requestBody,
+                statusCode=gu.RESPONSECODE_UNAUTHRORIZED,
+            )
+
         result = util.cognitoClient.admin_initiate_auth(
             AuthFlow="ADMIN_NO_SRP_AUTH",
             AuthParameters={
@@ -34,9 +45,20 @@ def authenticate_POST(util: gu.Global_Utility, requestBody: dict):
 
             response = (result, gu.RESPONSECODE_OK)
 
+    except gu.UptakeblueException:
+        raise
+
     except Exception as e:
+        code = gu.RESPONSECODE_SERVERERROR
+        if "NotAuthorizedException" in str(e):
+            e = Exception("Unauthorized")
+            code = gu.RESPONSECODE_UNAUTHRORIZED
+
         raise gu.UptakeblueException(
-            e, source=f"{MODULE}.authenticate_POST()", paramargs=requestBody
+            e,
+            source=f"{MODULE}.authenticate_POST()",
+            paramargs=requestBody,
+            statusCode=code,
         )
 
     return response
